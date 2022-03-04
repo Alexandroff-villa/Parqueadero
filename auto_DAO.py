b736165787e8a11daf65d49c032a44463f6de8a5
adr_alexander@hotmail.com <adr_alexander@hotmail.com>
2022-03-04 15:28:07 -0500

    Casi terminando con el nuevo metodo para conexion de datos

--- auto_DAO.py
+++ auto_DAO.py
@@ -1,6 +1,7 @@
-from conexion import Conexion
-from usuario import Usuario
 from logger import log
+import mysql.connector
+
+from usuario import Usuario 
 
 
 class UsuarioDAO:
@@ -9,38 +10,53 @@ class UsuarioDAO:
     CRUD (Create-Read-Update-Delete)
     '''
 
-    _SELECCIONAR = 'SELECT * FROM parqueadero ORDER BY id'
-    _INSERTAR = 'INSERT INTO parqueadero(placa,marca,color,fecha) VALUES(%s, %s, %s,%s)'
-    _ACTUALIZAR = 'UPDATE parqueadero SET placa=%s, marca=%s, color=%s WHERE id=%s'
-    _ELIMINAR = 'DELETE FROM parqueadero WHERE id=%s'
+    def __init__(self):
+        try:
+            self.conexion = mysql.connector.connect(
+                host = "localhost",
+                user = "root",
+                password = "23deAbril",
+                port = 3306,
+                db = "tienda"
+                )
+        except Exception as e:
+            print(f'Error no se pudo realozar la conexion: {e}')
 
-    @classmethod
-    def seleccionar(cls):
-        with Conexion.obtenerCursor() as cursor:
-            cursor.execute(cls._SELECCIONAR)
-            registros = cursor.fetchall()
-            Usuarios = []
-            for registro in registros:
-                usuario = Usuario(registro[0],registro[1],registro[2],registro[3],registro[4])
-                Usuarios.append(usuario)
-            return Usuarios
 
-    @classmethod
-    def insertar(cls,usuario):
-        with Conexion.obtenerConexion() as conexion:
-            with conexion.cursor() as cursor:
-                valores = (usuario.placa, usuario.marca, usuario.color, usuario.fecha)
-                cursor.execute(cls._INSERTAR, valores)
-                log.debug(f'persona a insetar: {usuario}')
-                return cursor.rowcount
+
+        self.SELECCIONAR = 'SELECT * FROM parqueadero ORDER BY id'
+        self.INSERTAR = "INSERT INTO parqueadero(placa,marca,color,fecha) VALUES (%s, %s, %s, %s)"
+
+    def seleccionar(self):
+        if self.conexion.is_connected():
+            try: 
+                cursor = self.conexion.cursor()
+                cursor.execute(self.SELECCIONAR)
+                registros = cursor.fetchall()
+                Usuarios = []
+                for registro in registros:
+                    usuario = Usuario(registro[0],registro[1],registro[2],registro[3],registro[4])
+                    Usuarios.append(usuario)
+                return Usuarios
+            except Exception as e:
+                print(f'Ocurrio un error al seleccionar: {e}')
+
+    def insertar(self,usuario):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                values = (usuario.placa,usuario.marca,usuario.color,usuario.fecha)
+                cursor.execute(self.INSERTAR,values)
+                self.conexion.commit()
+            except Exception as e:
+                print(f'Ocurrio un error al inertar un usuario: {e}')
 
 if __name__ == '__main__':
     #insertar objeto
-    usuario1 = Usuario(placa="JJJ-123",marca="Hino",color="verde",fecha="02/03/2022")
-    usuarios_insertados = UsuarioDAO.insertar(usuario1)
-    log.debug(f'Usuarios insertadas: {usuarios_insertados}')
+    # usuario1 = Usuario(placa="JJJ-123",marca="Hino",color="verde",fecha="02/03/2022")
+    # usuarios_insertados = UsuarioDAO().insertar(usuario1)
 
     #Seleccionar objetos
-    usuarios = UsuarioDAO.seleccionar()
+    usuarios = UsuarioDAO().seleccionar()
     for usuario in usuarios:
         log.debug(usuario)
